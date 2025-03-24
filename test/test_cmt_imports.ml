5656a335bb81df041a930fc9512a821240aefa3b
mununki <woonki.moon@gmail.com>
2025-03-24 15:12:09 +0900

    tests

---
+++ test/test_cmt_imports.ml
@@ -0,0 +1,127 @@
+open Stdlib
+open Printf
+
+(* Define stdlib_modules list - taken from parser.ml *)
+let stdlib_modules =
+  [
+    (* ReScript standard library modules *)
+    "Js";
+    "Belt";
+    "Belt_Int";
+    "Belt_internals";
+    "Array";
+    "List";
+    "String";
+    "Bytes";
+    "Printexc";
+    "Printf";
+    "Stdlib";
+    "Pervasives";
+    "PervasivesU";
+    (* ReScript compiler internal modules *)
+    "Caml1999I022";
+    "Has_arity1";
+    "Has_arity2";
+    "DA";
+    "GitHub";
+    "Users";
+    "TH";
+    "BZ";
+    "BZf";
+    "Bgk";
+    "Bgq";
+    (* Additional internal modules *)
+    "CamlinternalLazy";
+    "CamlinternalFormat";
+    "CamlinternalOO";
+    "Js_array";
+    "Js_string";
+    "Js_dict";
+    "Js_json";
+    "Js_math";
+    "Js_date";
+    "Js_global";
+    "Js_obj";
+    "Js_typed_array";
+    "Js_promise";
+    "Js_null";
+    "Js_undefined";
+    "Js_null_undefined";
+    (* Additional standard modules *)
+    "Int64";
+    "Obj";
+    "React";
+    "ReactEvent";
+    "Dom";
+    "Fetch";
+    "ReactNavigation";
+    "WebView";
+    "ReactNative";
+  ]
+
+(* Function to check if a module is stdlib or internal - taken from parser.ml & extended *)
+let is_stdlib_or_internal_module name =
+  List.exists
+    (fun m ->
+      String.lowercase_ascii name = String.lowercase_ascii m
+      || String.starts_with ~prefix:"Caml" name
+      || String.starts_with ~prefix:"Js_" name
+      || String.starts_with ~prefix:"Belt_" name)
+    stdlib_modules
+  || String.starts_with ~prefix:"Core__"
+       name (* Exclude Core__ prefix modules *)
+  || String.starts_with ~prefix:"Jsx" name
+
+(* Exclude Jsx related modules *)
+(* Exclude *_Query_* modules *)
+
+(* Test execution function *)
+let test_cmt_imports () =
+  let cmt_file = "test/fixtures/WebView.cmt" in
+
+  printf "=== Testing cmt_imports for WebView.cmt file ===\n";
+
+  try
+    (* Read cmt file *)
+    let cmt_info = Cmt_format.read_cmt cmt_file in
+
+    (* Get cmt_imports list *)
+    let imports = cmt_info.Cmt_format.cmt_imports in
+
+    (* Filter imports excluding std modules *)
+    let filtered_imports =
+      List.filter
+        (fun (module_name, _) -> not (is_stdlib_or_internal_module module_name))
+        imports
+    in
+
+    (* Output results *)
+    printf "cmt_imports found in WebView.cmt file excluding stdlib modules:\n";
+    List.iter
+      (fun (module_name, digest_opt) ->
+        match digest_opt with
+        | Some digest ->
+            let digest_str = Digest.to_hex digest in
+            printf "  - %s (digest: %s)\n" module_name digest_str
+        | None -> printf "  - %s (digest: none)\n" module_name)
+      filtered_imports;
+
+    printf "Total of %d import modules, %d non-stdlib modules found.\n"
+      (List.length imports)
+      (List.length filtered_imports);
+    true
+  with
+  | Cmt_format.Error msg ->
+      printf "Error: %s\n" msg;
+      false
+  | Sys_error msg ->
+      printf "System error: %s\n" msg;
+      false
+  | e ->
+      printf "Unexpected error: %s\n" (Printexc.to_string e);
+      false
+
+(* Main execution code *)
+let () =
+  let success = test_cmt_imports () in
+  exit (if success then 0 else 1)
---
+++ test/test_cmt_values.ml
@@ -0,0 +1,98 @@
+open Stdlib
+open Printf
+
+(** * Module to test cmt_value_dependencies from cmt files. *
+    cmt_value_dependencies contains value dependency information tracked by the
+    OCaml type compiler. * This shows how functions or values depend on other
+    functions or values. *)
+
+(* Function to test value_dependencies of a specific cmt file *)
+let test_cmt_file_values cmt_file =
+  printf "=== Testing cmt_value_dependencies for %s file ===\n"
+    (Filename.basename cmt_file);
+
+  try
+    (* Read cmt file to extract information *)
+    let cmt_info = Cmt_format.read_cmt cmt_file in
+
+    (* Get cmt_value_dependencies list
+       This list consists of (source value, target value) pairs for each dependency *)
+    let value_deps = cmt_info.Cmt_format.cmt_value_dependencies in
+
+    (* Get module name *)
+    let module_name = cmt_info.Cmt_format.cmt_modname in
+
+    (* Output results *)
+    printf "cmt_value_dependencies found in module %s (file %s):\n" module_name
+      (Filename.basename cmt_file);
+
+    if List.length value_deps = 0 then printf "  No value dependencies found.\n"
+    else
+      List.iter
+        (fun (val_desc1, val_desc2) ->
+          printf "  - Value dependency found:\n";
+
+          (* Print value type information *)
+          printf "    [Source Value]\n";
+          printf "    Type: %s\n"
+            (match val_desc1.Types.val_kind with
+            | Types.Val_reg -> "Regular value" (* Regular function or value *)
+            | Types.Val_prim s ->
+                "Primitive: " ^ s (* Built-in function/operation *)
+            | Types.Val_ivar (mflag, s) ->
+                let mutability =
+                  match mflag with
+                  | Types.Immutable -> "Immutable"
+                  | Types.Mutable -> "Mutable"
+                in
+                Printf.sprintf "Instance variable(%s): %s" mutability s
+            | Types.Val_self _ -> "Self reference"
+            | Types.Val_anc _ -> "Ancestor reference"
+            | Types.Val_unbound -> "Unbound value");
+
+          printf "    [Target Value]\n";
+          printf "    Type: %s\n"
+            (match val_desc2.Types.val_kind with
+            | Types.Val_reg -> "Regular value"
+            | Types.Val_prim s -> "Primitive: " ^ s
+            | Types.Val_ivar (mflag, s) ->
+                let mutability =
+                  match mflag with
+                  | Types.Immutable -> "Immutable"
+                  | Types.Mutable -> "Mutable"
+                in
+                Printf.sprintf "Instance variable(%s): %s" mutability s
+            | Types.Val_self _ -> "Self reference"
+            | Types.Val_anc _ -> "Ancestor reference"
+            | Types.Val_unbound -> "Unbound value");
+
+          printf "\n")
+        value_deps;
+
+    printf "Total of %d value dependencies found.\n\n" (List.length value_deps);
+
+    true
+  with
+  | Cmt_format.Error msg ->
+      printf "Error: %s\n" msg;
+      false
+  | Sys_error msg ->
+      printf "System error: %s\n" msg;
+      false
+  | e ->
+      printf "Unexpected error: %s\n" (Printexc.to_string e);
+      false
+
+(* Main function to test multiple cmt files *)
+let test_cmt_values () =
+  let files =
+    [ "test/fixtures/WebView.cmt"; "test/fixtures/FollowUtils.cmt" ]
+  in
+
+  let results = List.map test_cmt_file_values files in
+  List.for_all (fun x -> x) results
+
+(* Main execution code *)
+let () =
+  let success = test_cmt_values () in
+  exit (if success then 0 else 1)
