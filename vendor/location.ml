f978746860c86da89a95afc22b6031ed0c5651e3
mununki <woonki.moon@gmail.com>
2025-03-26 15:32:42 +0900

    Remove unused AST types, identifier, and location modules to streamline the codebase.

--- vendor/location.ml
+++
@@ -1,8 +0,0 @@
-type t = {
-  loc_start : Lexing.position;
-  loc_end : Lexing.position;
-  loc_ghost : bool;
-}
-
-let none =
-  { loc_start = Lexing.dummy_pos; loc_end = Lexing.dummy_pos; loc_ghost = true }
